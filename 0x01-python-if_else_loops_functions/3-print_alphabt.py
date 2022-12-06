60767cc7f2c6f2719d7f1ed42310955595603e49
christian <horpeyemijoshua@gmail.com>
2022-12-07 00:53:43 +0100

    python solution

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if chr[i] != 'q' and chr[i] != 'e':
+    if chr(i) != 'e' and chr(i) != 'q':
         print('{:c}'.format(i), end='')
