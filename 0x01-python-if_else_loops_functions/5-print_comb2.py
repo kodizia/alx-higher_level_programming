b551316082edf799a2d07071a762f0c164ca6f20
user.name:"kodizia <user.email: "horpeyemijoshua@gmail.com>
2023-12-01 21:37:30 +0100

    added solution to task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
-for i in range(0, 100):
-    if i == 99:
-        print("{:02d}".format(i))
+#!/usr/bin/python3
+for _ in range(0, 100):
+    if _ == 99:
+        print("{:02d}".format(_))
     else:
-        print("{:02d}".format(i), end=', ')
+        print("{:02d}".format(_), end=', ')
