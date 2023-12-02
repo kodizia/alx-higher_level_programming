a4d27928f5edbf15efac43c8566ff5e9cda8f629
user.name:"kodizia <user.email: "horpeyemijoshua@gmail.com>
2023-12-02 03:36:28 +0100

    added solution to task 7 to convert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for  j in str:
+        if ord(j) >= 97 and ord(j) <= 122:
+            print("{}".format(chr(ord(j) - 32)), end='')
+        else:
+            print("{}".format(j), end = "")
+    print("")
