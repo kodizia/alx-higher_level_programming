83ed43f2bb02269f31c8de14e06fadd5a99dd491
user.name:"kodizia <user.email: "horpeyemijoshua@gmail.com>
2023-12-19 03:56:48 +0100

    added solution to tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct, *args):
     import sys
     try:
         return fct(*args)
-    
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
+    
