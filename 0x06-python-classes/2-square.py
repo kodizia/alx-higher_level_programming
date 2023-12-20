07dac0f47936d10f3cc65edb6486943f7590cd3c
user.name:"kodizia <user.email: "horpeyemijoshua@gmail.com>
2023-12-20 14:25:57 +0100

    added solution to task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 0-square.py by kodizia
+"""Defines a square """
+
+class Square:
+    """A Square class"""
+    
+    def __init__(self, size=0):
+        """Initialize the square.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
