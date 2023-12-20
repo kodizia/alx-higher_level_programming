d50280fc348245851618db5c4a20e2d465565c80
user.name:"kodizia <user.email: "horpeyemijoshua@gmail.com>
2023-12-20 13:52:49 +0100

    added solution to task zero

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class square:
+    """A square object class that
+    can be used to represent a square"""
+    pass
---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class square:
+    """ This Class defines a square"""
+    pass
