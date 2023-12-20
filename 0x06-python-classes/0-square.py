cfd2f7b302fcb99d9b3c4fbc65d17d5a346c2021
user.name:"kodizia <user.email: "horpeyemijoshua@gmail.com>
2023-12-20 13:57:02 +0100

    added solution to task zero

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-#0-square.py by medici
-"""Defines a square object class"""
+# 0-square.py medici
+"""Defines a square """
+
 class Square:
-    """ Does nothing"""
+    """Does nothing"""
     pass
